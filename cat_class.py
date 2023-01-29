3d9706c77fbc1c97d642eb4d9e38de2a348315df
OG-QAP105 <119675639+OG-QAP105@users.noreply.github.com>
2023-01-29 13:34:02 +0200

    Update cat_class.py

--- cat_class.py
+++ cat_class.py
@@ -10,13 +10,4 @@ class Cat:
         self.gender = gender
         self.age = age
 
-    def getName(self):
-        return self.name
-
-    def getGender(self):
-        return self.gender
-
-    def getAge(self):
-        return self.age
-
 
